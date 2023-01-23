ddebb241788f75db02a358d1e74c9dee8c343d54
Bruno da Silva Vieira <brup12.v@gmail.com>
2023-01-22 22:44:21 -0300

    update

--- exercicios-praticos/nlw-setup/script.js
+++ exercicios-praticos/nlw-setup/script.js
@@ -1,11 +1,18 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header')
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  water: ["01-03", "01-05", "01-07", "01-08"],
-  takePills: ["01-04"],
+button.addEventListener('click', add)
+
+function add() {
+  alert('hello world')
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//   run: ["01-01", "01-02", "01-06"],
+//   water: ["01-03", "01-05", "01-07", "01-08"],
+//   takePills: ["01-04"],
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
